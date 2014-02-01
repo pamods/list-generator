91c44f13960922fd4b444f93567e47969d810d4d
pieter vanh <pieter.vanh@gmail.com>
2014-02-01 13:20:07 +0100

    don't bomb on mods with broken modinfo.json

--- ListGenerator/Program.cs
+++ ListGenerator/Program.cs
@@ -10,104 +10,112 @@ using Newtonsoft.Json.Linq;
 
 namespace ListGenerator
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			new Program().Run();
-		}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
 
-		public static string BaseModUrl
-		{
-			get { return ConfigurationManager.AppSettings["baseModUrl"]; }
-		}
+        public static string BaseModUrl
+        {
+            get { return ConfigurationManager.AppSettings["baseModUrl"]; }
+        }
 
-		private void Run()
-		{
-			var modList = LoadMods();
+        private void Run()
+        {
+            var modList = LoadMods();
 
-			GenerateJSONFileNewtonSoft(modList);
-		}
+            GenerateJSONFileNewtonSoft(modList);
+            //Console.ReadLine();
+        }
 
-		private List<JObject> LoadMods()
-		{
-			var res = new List<JObject>();
+        private List<JObject> LoadMods()
+        {
+            var res = new List<JObject>();
 
-			foreach (var filename in Directory.EnumerateFiles("user_mods", "*.zip"))
-			{
-				try
-				{
-					var zip = new ZipFile(filename);
-					var zipJSONFile = FindJSONFile(zip);
+            foreach (var filename in Directory.EnumerateFiles("user_mods", "*.zip"))
+            {
+                try
+                {
+                    var zip = new ZipFile(filename);
+                    var zipJSONFile = FindJSONFile(zip);
 
-					if (zipJSONFile == null)
-						throw new Exception("Unable to find modinfo.json file");
+                    if (zipJSONFile == null)
+                        throw new Exception("Unable to find modinfo.json file");
 
-					var stream = zip.GetInputStream(zipJSONFile);
-					var memoryStream = new MemoryStream();
-					StreamUtils.Copy(stream, memoryStream, new byte[4196]);
+                    var stream = zip.GetInputStream(zipJSONFile);
+                    var memoryStream = new MemoryStream();
+                    StreamUtils.Copy(stream, memoryStream, new byte[4196]);
 
-					var jsonFileContents = Encoding.UTF8.GetString(memoryStream.GetBuffer());
-					JObject mod = JObject.Parse(jsonFileContents);
+                    var jsonFileContents = Encoding.UTF8.GetString(memoryStream.GetBuffer());
+                    JObject mod = JObject.Parse(jsonFileContents);
 
-					//Add missing things
-					mod.Add("url", BaseModUrl + new FileInfo(filename).Name);
+                    //Add missing things
+                    mod.Add("url", BaseModUrl + new FileInfo(filename).Name);
 
-					//Remove things we dont want
-					mod.Remove("start");
-					mod.Remove("new_game");
-					mod.Remove("lobby");
-					mod.Remove("live_game");
-					mod.Remove("settings");
-					mod.Remove("system_editor");
-					mod.Remove("global_mod_list");
-					mod.Remove("server_browser");
-					mod.Remove("social");
-					mod.Remove("game_over");
+                    //Remove things we dont want
+                    mod.Remove("start");
+                    mod.Remove("new_game");
+                    mod.Remove("lobby");
+                    mod.Remove("live_game");
+                    mod.Remove("settings");
+                    mod.Remove("system_editor");
+                    mod.Remove("global_mod_list");
+                    mod.Remove("server_browser");
+                    mod.Remove("social");
+                    mod.Remove("game_over");
 
-					mod.Remove("priority");
-					mod.Remove("enabled");
-					mod.Remove("context");
-					mod.Remove("identifier");
-					mod.Remove("signature");
+                    mod.Remove("priority");
+                    mod.Remove("enabled");
+                    mod.Remove("context");
+                    mod.Remove("identifier");
+                    mod.Remove("signature");
 
-					res.Add(mod);
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine("Failed reading {0}, Error: {1}", filename, ex);
-				}
-			}
+                    res.Add(mod);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed reading {0}, Error: {1}", filename, ex);
+                }
+            }
 
-			return res;
-		}
+            return res;
+        }
 
-		private ZipEntry FindJSONFile(ZipFile zip)
-		{
-			foreach (ZipEntry entry in zip)
-			{
-				if (entry.Name.EndsWith("/modinfo.json", StringComparison.InvariantCultureIgnoreCase))
-				{
-					return entry;
-				}
-			}
-			return null;
-		}
+        private ZipEntry FindJSONFile(ZipFile zip)
+        {
+            foreach (ZipEntry entry in zip)
+            {
+                if (entry.Name.EndsWith("/modinfo.json", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return entry;
+                }
+            }
+            return null;
+        }
 
-		private void GenerateJSONFileNewtonSoft(List<JObject> modList)
-		{
-			using (StreamWriter sw = new StreamWriter("modlist.json"))
-			{
-				var dict = new SortedDictionary<string, JObject>();
-				foreach (var mod in modList)
-				{
-					dict.Add((string)mod.GetValue("id"), mod);
-					mod.Remove("id");
-				}
+        private void GenerateJSONFileNewtonSoft(List<JObject> modList)
+        {
+            using (StreamWriter sw = new StreamWriter("modlist.json"))
+            {
+                var dict = new SortedDictionary<string, JObject>();
+                foreach (var mod in modList)
+                {
+                    try
+                    {
+                        dict.Add((string)mod.GetValue("id"), mod);
+                        mod.Remove("id");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
 
-				sw.Write(JsonConvert.SerializeObject(dict, Formatting.Indented));
-				sw.Close();
-			}
-		}
-	}
+                sw.Write(JsonConvert.SerializeObject(dict, Formatting.Indented));
+                sw.Close();
+            }
+        }
+    }
 }
